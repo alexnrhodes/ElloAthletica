c98e7b2ab846360aa0654ddc6e780fdcd12ffe77
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-12-03 12:06:18 -0600

    Added Xcode Project to Repository

---
+++ ElloAthletica/ElloAthletica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElloAthletica
+//
+//  Created by Alex Rhodes on 12/3/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
